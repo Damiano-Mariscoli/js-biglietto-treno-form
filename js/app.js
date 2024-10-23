477fe3d0e2d23fb6f478cc614d6862692a41143a
Damiano-Mariscoli <damiano.mariscoli2@gmail.com>
2024-10-23 16:27:13 +0200

    ADDED: funzionamento principale del codice

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 form.addEventListener('submit', function (event) {
     event.preventDefault();
     const ageField = document.getElementById('input-age');
@@ -8,22 +9,33 @@ form.addEventListener('submit', function (event) {
 
 
     if (age < 18) {
-        
+
         scontoAdvanced = (prezzo) * 20 / 100
-        
+
         console.log('hai diritto allo sconto giovani!!')
-        
+
     } else if (age >= 65) {
-        
+
         scontoAdvanced = (prezzo) * 40 / 100
-        
+
         console.log('hai diritto allo sconto over 65!!')
-        
+
     }
     prezzo = prezzo - scontoAdvanced
-    
-    console.log(`il prezzo del tuo biglietto è: ${prezzo.toFixed(2)}$`)
-    
-})
 
+    console.log(`il prezzo del tuo biglietto è: ${prezzo.toFixed(2)}$`)
+
+
+    const textKm = document.createElement('p');
+    textKm.textContent = `hai scelto ${kmField.value} KM`;
+    document.body.appendChild(textKm);
+
+    const textAge = document.createElement('p');
+    textAge.textContent = `hai inserito ${ageField.value} anni`
+    document.body.appendChild(textAge)
+
+    const textPrice = document.createElement('p')
+    textPrice.textContent = `il prezzo del tuo biglietto è ${prezzo} $`
+    document.body.appendChild(textPrice)
+})
 
